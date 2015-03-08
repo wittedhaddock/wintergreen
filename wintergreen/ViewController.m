b9b6afecbeba3dbd542fcd90d7e1f636b6a90307
James William Graham <wittedhaddock@gmail.com>
2015-03-08 16:19:47 -0400

    gyro data viewable on screen

--- wintergreen/ViewController.m
+++ wintergreen/ViewController.m
@@ -7,16 +7,63 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
+@property (strong, nonatomic) CMMotionManager *manager;
+
+//gyro
+@property (weak, nonatomic) IBOutlet UILabel *gX;
+@property (weak, nonatomic) IBOutlet UILabel *gY;
+@property (weak, nonatomic) IBOutlet UILabel *gZ;
+
+//accel
+@property (weak, nonatomic) IBOutlet UILabel *aX;
+@property (weak, nonatomic) IBOutlet UILabel *aY;
+@property (weak, nonatomic) IBOutlet UILabel *aZ;
 
 @end
 
 @implementation ViewController
 
+- (CMMotionManager *)manager{
+    if(!_manager){
+        _manager = [[CMMotionManager alloc] init];
+    }
+    return _manager;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    if ([self.manager isGyroAvailable] && [self.manager isGyroActive] == NO) {
+        [self startGryoUpdate];
+    }
+}
+
+- (void)startGryoUpdate{
+    [self.manager setGyroUpdateInterval:0.01f];
+    [self.manager startGyroUpdatesToQueue:[NSOperationQueue mainQueue]
+                              withHandler:^(CMGyroData *gyroData, NSError *error) {
+                                  if (!error) {
+                                      [self processGyroData:gyroData];
+                                  }
+                                  else {
+                                      NSLog(@"ERROR:\n%@", [error userInfo]);
+                                  }
+                              }];
+}
+- (void)processGyroData:(CMGyroData *)data{
+    double x = data.rotationRate.x;
+    double y = data.rotationRate.y;
+    double z = data.rotationRate.z;
+    [self updateViewWithNewGyroDataX:x Y:y Z:z];
+}
+
+- (void)updateViewWithNewGyroDataX:(double)x Y:(double)y Z:(double)z{
+    self.gX.text = [NSString stringWithFormat:@"X: %f", x];
+    self.gY.text = [NSString stringWithFormat:@"Y: %f", y];
+    self.gZ.text = [NSString stringWithFormat:@"Z: %f", z];
 }
 
 - (void)didReceiveMemoryWarning {
