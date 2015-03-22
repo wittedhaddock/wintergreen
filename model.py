46f7eeec92256653a0dcf475aaace4e502dc6835
James William Graham <wittedhaddock@gmail.com>
2015-03-22 12:01:48 -0400

    accelerometer and gyroscope data plotted in a skewed sense (assuming time was uniform in its distribution)

--- model.py
+++ model.py
@@ -13,6 +13,7 @@ import pandas
 from sklearn.neighbors import KNeighborsClassifier
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 connection = sqlite3.connect("./device_data.db")
@@ -24,9 +25,13 @@ SELECT * FROM data
 """
 df = pandas.read_sql(query, connection)
 connection.close()
-x_ind = df.milliseconds
+x_ind = df.milliseconds.cumsum()
 x_ind.plot()
 
-x_df.plot()
+time_series = [i-x_ind[0] for i in x_ind]
+g_d = {"g_x" : df.g_x, "g_y" : df.g_y, "g_z" : df.g_z}
+g_df = pandas.DataFrame(data=g_d, index=range(len(time_series)))
 
+a_d = {"a_x" : df.a_x, "a_y" : df.a_y, "a_z" : df.a_z}
+a_df = pandas.DataFrame(data=a_d, index=range(len(time_series)))
 df.plot()
