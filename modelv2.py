28a6ab88edee7221ffffbe94cf708517b8eb620a
James William Graham <wittedhaddock@gmail.com>
2015-04-02 20:12:28 -0400

    modelv2 added

--- modelv2.py
+++ modelv2.py
@@ -31,13 +31,43 @@ connection.close()
 
 explanatory_header_names = ["milliseconds", "g_x", "g_y", "g_z", "a_x", "a_y", "a_z"]
 
-df_james = df[df.name == "James"]
-df_james = df_james[df_james.milliseconds - df_james.milliseconds[0] < 13000]
+df = df.replace(to_replace="James", value = 1, inplace=False)
+df = df.replace(to_replace="JonBlum", value = 2, inplace = False)
+df = df.replace(to_replace="Andrew", value = 3, inplace = False)
+
+
+df_james = df[df.name == 1]
+df_james = df_james[df_james.milliseconds - df_james.milliseconds[0] < 5000]
 df_james = df_james[df_james.milliseconds - df_james.milliseconds[0] > 4000]
 
 df_james.g_x.plot()
-df_james.g)
+df_james.g_y.plot()
+df_james.g_z.plot()
+
+df_james.a_x.plot()
+df_james.a_y.plot()
+df_james.a_z.plot()
+
+
+james_gyroscope_x = df_james.g_x
+def derivative(dataFrameColumn):
+    deltas = []
+    j = 0
+    while j < 1000:
+        j = j + 1
+        deltas.append( dataFrameColumn[j] - dataFrameColumn[j - 1])
+    return deltas
+    
+james_gyroscope_x_derivative = derivative(james_gyroscope_x)
+
+james_gyroscope_x_concavity = derivative(james_gyroscope_x_derivative)
+
+a_df = pandas.DataFrame(data=a_d, index=range(len(time_series)))
+james_gyroscope_x[:1000].plot()
+james_gryoscope_x_derivative_df = pandas.DataFrame(data=james_gyroscope_x_derivative, index = range(len(james_gyroscope_x_derivative) ))
+df_slice = df#[df.milliseconds - df.milliseconds[0] < 000]
+df_slice = df[df_slice.milliseconds - df.milliseconds[0] > 4000]
 
 kmeans_est = KMeans(n_clusters=3)
-kmeans_est.fit(df[["milliseconds", "g_x"]])
-plt.scatter(df.milliseconds, df.g_x, s=60, c=kmeans_est.labels_)
+kmeans_est.fit(df_slice)
+plt.scatter(df_slice.g_x, df_slice.name, s=60, c=kmeans_est.labels_)
