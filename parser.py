1b7e5b8298709286a39b564e9d6b8c3c3cc9c51e
James William Graham <wittedhaddock@gmail.com>
2015-04-04 11:10:27 -0400

    parser updated to create session number -- database regenerated

--- parser.py
+++ parser.py
@@ -5,26 +5,27 @@ import sqlite3
 import re
 
 def main():
-  if len(sys.argv) >= 3:
+  if len(sys.argv) >= 4:
     fileName = sys.argv[1]
     human_name = sys.argv[2]
+    session_number = sys.argv[3]
   else:
-    print "must pass file and human name"
+    print "must pass file, human name, and session number"
     return
   file = open(fileName, "rU")
   if file is None:
     print "cannot read file... corrupted?"
     return
   else:
-    formatAndWriteToDBWithFileWithHumansName(file, str(human_name))
+    formatAndWriteToDBWithFileWithHumansNameAndSessionNumber(file, str(human_name), session_number)
     print "working..."
   print "Finished!"
 
-def formatAndWriteToDBWithFileWithHumansName(file, name_of_human):
+def formatAndWriteToDBWithFileWithHumansNameAndSessionNumber(file, name_of_human, session_num):
   connection = sqlite3.connect(r"./device_data.db")
   cursor = connection.cursor()
   cursor.execute("""
-                  CREATE TABLE IF NOT EXISTS data (name text, milliseconds float, g_x float, g_y float, g_z float, a_x float, a_y float, a_z float)
+                  CREATE TABLE IF NOT EXISTS data (name text, milliseconds float, session_number int, g_x float, g_y float, g_z float, a_x float, a_y float, a_z float)
                  """)
   #insert data
   for line in file:
@@ -38,9 +39,9 @@ def formatAndWriteToDBWithFileWithHumansName(file, name_of_human):
     a_y = match[4].split()[1]
     a_z = match[5].split()[1]
     tim = match[6].split()[1]
-    t = (name_of_human, g_x, g_y, g_z, a_x, a_y, a_z, tim)
-    connection.execute("""INSERT INTO data (name, g_x, g_y, g_z, a_x, a_y, a_z, milliseconds) 
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)""" , t )
+    t = (name_of_human, g_x, g_y, g_z, a_x, a_y, a_z, tim, session_num)
+    connection.execute("""INSERT INTO data (name, g_x, g_y, g_z, a_x, a_y, a_z, milliseconds, session_number) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""" , t )
 
   connection.commit()
   connection.close()
